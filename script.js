bdfb53df85f9c716c877894027c26a89dfd74d97
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-16 07:03:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let gUrl = window.location.href;
-    console.log(gUrl);
+let gUrl = window.location.href;   
     document.getElementById("Get_url").innerHTML = 
-    "The length of the URL is: " + gUrl.length;
+    "The length of the URL is: " + gUrllen;
